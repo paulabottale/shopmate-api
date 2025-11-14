57f6a9a93f142a0689d4828e0043f16587acfaf5
paulabottale.dev <paulabottale.dev@gmail.com>
2025-11-14 10:25:07 -0300

    deployment in Vercel

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,15 +1,5 @@
 import "dotenv/config";
 
-type StringValue =
-  | `${number}`
-  | `${number}ms`
-  | `${number}s`
-  | `${number}m`
-  | `${number}h`
-  | `${number}d`;
-
-export type ExpiresIn = number | StringValue;
-
 export const env = {
   port: Number(process.env.PORT || 3000),
   nodeEnv: process.env.NODE_ENV || "development",
@@ -18,7 +8,6 @@ export const env = {
   accessSecret: process.env.JWT_ACCESS_SECRET || "dev_access",
   refreshSecret: process.env.JWT_REFRESH_SECRET || "dev_refresh",
 
-
-  accessTtl: (process.env.JWT_ACCESS_EXPIRES || "15m") as ExpiresIn,
-  refreshTtl: (process.env.JWT_REFRESH_EXPIRES || "7d") as ExpiresIn
+  accessTtl: process.env.JWT_ACCESS_EXPIRES || "15m",
+  refreshTtl: process.env.JWT_REFRESH_EXPIRES || "7d"
 };
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,16 @@ import { connectDB } from "./config/connectDB";
 import { env } from "./config/env";
 
 async function boot() {
-  await connectDB();
-  app.listen(env.port, () => {
-    console.log(`API lista en http://localhost:${env.port}`);
-  });
+  try {
+    await connectDB();
+    app.listen(env.port, () => {
+      console.log(`API lista en http://localhost:${env.port}`);
+    });
+  } catch (err) {
+    console.error("Error al iniciar el servidor:", err);
+    process.exit(1);
+  }
 }
+
 boot();
+
